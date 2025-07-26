969a3764e258d459a743e742d7cd36458095f03d
deliasoare <deliasoare006@yahoo.com>
2025-07-26 10:32:53 +0300

    Style global header

--- frontend/src/components/Bucketlist.jsx
+++ frontend/src/components/Bucketlist.jsx
@@ -55,12 +55,10 @@ const Bucketlist = ({ isLogged }) => {
     }
       }, []);
   
-  // Show loading animation while checking authentication
   if (isLoading) {
     return <PackingLoader />;
   }
 
-  // Show error page only if user is definitely not authenticated
   if (!isAuthenticated || !profileInfo) {
     return <ErrorPage />;
   }
